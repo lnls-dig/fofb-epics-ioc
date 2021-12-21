4b7adc53354cfed0838a8260131fcd07d4857896
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2021-12-21 12:43:17 -0200

    FOFBApp and iocFOFB: update waveform files

--- iocBoot/iocFOFB/waveformFilePlugins.cmd
+++ iocBoot/iocFOFB/waveformFilePlugins.cmd
@@ -1,1 +1,15 @@
+##################### GEN Data File Saving ##########################
 
+NDFileNetCDFConfigure("GEN_ABCD_FileNetCDF", $(QSIZE), 0, "$(PORT)", 4)
+dbLoadRecords("$(AREA_DETECTOR_COMPAT)/db/NDFileNetCDF.template","P=${P},R=${R}GEN_ABCD_NETCDF,PORT=GEN_ABCD_FileNetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=4")
+
+NDFileHDF5Configure("GEN_ABCD_FileHDF5", $(QSIZE), 0, "$(PORT)", 4)
+dbLoadRecords("$(AREA_DETECTOR_COMPAT)/db/NDFileHDF5.template",  "P=${P},R=${R}GEN_ABCD_HDF5,PORT=GEN_ABCD_FileHDF5,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=4")
+
+##################### PM Data File Saving ##########################
+
+NDFileNetCDFConfigure("PM_ABCD_FileNetCDF", $(QSIZE), 0, "$(PORT)", 10)
+dbLoadRecords("$(AREA_DETECTOR_COMPAT)/db/NDFileNetCDF.template","P=${P},R=${R}PM_ABCD_NETCDF,PORT=PM_ABCD_FileNetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=10")
+
+NDFileHDF5Configure("PM_ABCD_FileHDF5", $(QSIZE), 0, "$(PORT)", 10)
+dbLoadRecords("$(AREA_DETECTOR_COMPAT)/db/NDFileHDF5.template",  "P=${P},R=${R}PM_ABCD_HDF5,PORT=PM_ABCD_FileHDF5,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=10")
--- iocBoot/iocFOFB/waveformPlugins.cmd
+++ iocBoot/iocFOFB/waveformPlugins.cmd
@@ -1,3 +1,8 @@
 ##################### FOFB configuration ##########################
 
 dbLoadRecords("${TOP}/db/FOFB.template", "P=${P}, R=${R}, GLOBAL_ARRAY_NAME_X=X, GLOBAL_ARRAY_NAME_Y=Y, GLOBAL_ARRAY_NAME_XY=XY, GLOBAL_ARRAY_NAME_Q=Q, GLOBAL_ARRAY_NAME_SUM=SUM, PORT=$(PORT), ADDR=0, TIMEOUT=1")
+
+##################### GEN Data Waveforms ###########################
+
+NDStdArraysConfigure("GEN_CH0_Array", $(QSIZE), 0, "$(PORT)", 0)
+dbLoadRecords("$(AREA_DETECTOR_COMPAT)/db/NDStdArrays.template", "P=${P},R=${R}GEN_CH0,PORT=GEN_CH0_Array,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0,TYPE=Int32,FTVL=LONG,NELEMENTS=$(WAVEFORM_MAX_POINTS)")
