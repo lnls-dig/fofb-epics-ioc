687355b5306f40bc44063cbf784ea747288db7c0
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2022-01-25 13:53:30 -0300

    FOFBApp: add fofb_ctrl functions

--- FOFBApp/src/drvFOFB.h
+++ FOFBApp/src/drvFOFB.h
@@ -392,6 +392,48 @@ private:
 #define P_TriggerTrnSrcString                   "TRIGGER_TRN_SRC"                           /* asynUInt32Digital,      r/w */
 #define P_TriggerRcvInSelString                 "TRIGGER_RCV_IN_SEL"                        /* asynUInt32Digital,      r/w */
 #define P_TriggerTrnOutSelString                "TRIGGER_TRN_OUT_SEL"                       /* asynUInt32Digital,      r/w */
+#define P_FofbProcessingRamWriteString          "FOFB_PROCESSING_RAM_WRITE"                 /* asynUInt32Digital,      r/w */
+#define P_FofbProcessingRamAddrString           "FOFB_PROCESSING_RAM_ADDR"                  /* asynUInt32Digital,      r/w */
+#define P_FofbProcessingRamDataInString         "FOFB_PROCESSING_RAM_DATA_IN"               /* asynUInt32Digital,      r/w */
+#define P_FofbProcessingRamDataOutString        "FOFB_PROCESSING_RAM_DATA_OUT"              /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlActPartString                 "FOFB_CC_CFG_VAL_ACT_PART"                  /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlErrClrString                  "FOFB_CC_CFG_VAL_ERR_CLR"                   /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlCcEnableString                "FOFB_CC_CFG_VAL_CC_ENABLE"                 /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlTfsOverrideString             "FOFB_CC_CFG_VAL_TFS_OVERRIDE"              /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlBpmIdString                   "FOFB_CC_BPM_ID"                            /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlTimeFrameLenString            "FOFB_CC_TIME_FRAME_LEN"                    /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlMgtPowerdownString            "FOFB_CC_MGT_POWERDOWN"                     /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlMgtLoopbackString             "FOFB_CC_MGT_LOOPBACK"                      /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlTimeFrameDlyString            "FOFB_CC_TIME_FRAME_DLY"                    /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlGoldenOrbXString              "FOFB_CC_GOLDEN_ORB_X"                      /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlGoldenOrbYString              "FOFB_CC_GOLDEN_ORB_Y"                      /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlCustFeatureString             "FOFB_CC_CUST_FEATURE"                      /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlRxPolarityString              "FOFB_CC_RXPOLARITY"                        /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlPayloadselString              "FOFB_CC_PAYLOADSEL"                        /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlFofbdataselString             "FOFB_CC_FOFBDATASEL"                       /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlFirmwareVerString             "FOFB_CC_FIRMWARE_VER"                      /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlSysStatusString               "FOFB_CC_SYS_STATUS"                        /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlLinkPartnerString             "FOFB_CC_LINK_PARTNER_1"                    /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlLinkUpString                  "FOFB_CC_LINK_UP"                           /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlTimeFrameCountString          "FOFB_CC_TIME_FRAME_COUNT"                  /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlHardErrCntString              "FOFB_CC_HARD_ERR_CNT_1"                    /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlSoftErrCntString              "FOFB_CC_SOFT_ERR_CNT_1"                    /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlFrameErrCntString             "FOFB_CC_FRAME_ERR_CNT_1"                   /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlRxPckCntString                "FOFB_CC_RX_PCK_CNT_1"                      /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlTxPckCntString                "FOFB_CC_TX_PCK_CNT_1"                      /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlFodProcessTimeString          "FOFB_CC_FOD_PROCESS_TIME"                  /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlBpmCntString                  "FOFB_CC_BPM_COUNT"                         /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlBpmIdRdbackString             "FOFB_CC_BPM_ID_RDBACK"                     /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlTfLengthRdbackString          "FOFB_CC_TF_LENGTH_RDBACK"                  /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlPowerdownRdbackString         "FOFB_CC_POWERDOWN_RDBACK"                  /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlLoopbackRdbackString          "FOFB_CC_LOOPBACK_RDBACK"                   /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlFaivalRdbackString            "FOFB_CC_FAIVAL_RDBACK"                     /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlToaRdEnString                 "FOFB_CC_TOA_CTL_RD_EN"                     /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlToaRdStrString                "FOFB_CC_TOA_CTL_RD_STR"                    /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlToaDataString                 "FOFB_CC_TOA_DATA_VAL"                      /* asynUInt32Digital,      r/o */
+#define P_FofbCtrlRcbRdEnString                 "FOFB_CC_RCB_CTL_RD_EN"                     /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlRcbRdStrString                "FOFB_CC_RCB_CTL_RD_STR"                    /* asynUInt32Digital,      r/w */
+#define P_FofbCtrlRcbDataString                 "FOFB_CC_RCB_DATA_VAL"                      /* asynUInt32Digital,      r/o */
 
 typedef enum {
     TRIG_ACQ_START,
@@ -552,7 +594,49 @@ class drvFOFB : public asynNDArrayDriver {
         int P_TriggerTrnSrc;
         int P_TriggerRcvInSel;
         int P_TriggerTrnOutSel;
-#define LAST_COMMAND P_TriggerTrnOutSel
+        int P_FofbProcessingRamWrite;
+        int P_FofbProcessingRamAddr;
+        int P_FofbProcessingRamDataIn;
+        int P_FofbProcessingRamDataOut;
+        int P_FofbCtrlActPart;
+        int P_FofbCtrlErrClr;
+        int P_FofbCtrlCcEnable;
+        int P_FofbCtrlTfsOverride;
+        int P_FofbCtrlBpmId;
+        int P_FofbCtrlTimeFrameLen;
+        int P_FofbCtrlMgtPowerdown;
+        int P_FofbCtrlMgtLoopback;
+        int P_FofbCtrlTimeFrameDly;
+        int P_FofbCtrlGoldenOrbX;
+        int P_FofbCtrlGoldenOrbY;
+        int P_FofbCtrlCustFeature;
+        int P_FofbCtrlRxPolarity;
+        int P_FofbCtrlPayloadsel;
+        int P_FofbCtrlFofbdatasel;
+        int P_FofbCtrlFirmwareVer;
+        int P_FofbCtrlSysStatus;
+        int P_FofbCtrlLinkPartner;
+        int P_FofbCtrlLinkUp;
+        int P_FofbCtrlTimeFrameCount;
+        int P_FofbCtrlHardErrCnt;
+        int P_FofbCtrlSoftErrCnt;
+        int P_FofbCtrlFrameErrCnt;
+        int P_FofbCtrlRxPckCnt;
+        int P_FofbCtrlTxPckCnt;
+        int P_FofbCtrlFodProcessTime;
+        int P_FofbCtrlBpmCnt;
+        int P_FofbCtrlBpmIdRdback;
+        int P_FofbCtrlTfLengthRdback;
+        int P_FofbCtrlPowerdownRdback;
+        int P_FofbCtrlLoopbackRdback;
+        int P_FofbCtrlFaivalRdback;
+        int P_FofbCtrlToaRdEn;
+        int P_FofbCtrlToaRdStr;
+        int P_FofbCtrlToaData;
+        int P_FofbCtrlRcbRdEn;
+        int P_FofbCtrlRcbRdStr;
+        int P_FofbCtrlRcbData;
+#define LAST_COMMAND P_FofbCtrlRcbData
 
     private:
         /* Our data */
