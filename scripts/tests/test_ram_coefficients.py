99387cabd01a5372a6cde7fa8d0eb45d43494a44
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2022-01-31 15:24:05 -0300

    scripts: update test files

--- scripts/tests/test_ram_coefficients.py
+++ scripts/tests/test_ram_coefficients.py
@@ -2,7 +2,7 @@ from epics import PV
 import numpy as np
 from time import sleep
 
-pv_prefix = "XX-22SL19"
+pv_prefix = "XX-99SL01"
 
 ram_addr    = np.loadtxt('ram_addr.txt')
 ram_data_in = np.loadtxt('ram_data_in.txt')
