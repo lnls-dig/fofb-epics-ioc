13a4f3f48c2b57740cabf0bf50c98328b2a24be4
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2022-02-17 14:44:29 -0300

    FOFBApp: update waveform parameters

--- FOFBApp/src/drvFOFB.cpp
+++ FOFBApp/src/drvFOFB.cpp
@@ -69,7 +69,7 @@ typedef struct {
 
 static const channelMap_t channelMap[CH_END] = {
     /* [CH_DCC_FMC] =  */ {CH_HW_DCC_FMC,                       // HwDataChannel
-                            {WVF_DATA_DCC_FMC_CH0,
+                           {WVF_DATA_DCC_FMC_CH0,
                             WVF_DATA_DCC_FMC_CH1,
                             WVF_DATA_DCC_FMC_CH2,
                             WVF_DATA_DCC_FMC_CH3,
@@ -78,6 +78,15 @@ static const channelMap_t channelMap[CH_END] = {
                             WVF_DATA_DCC_FMC_CH6,
                             WVF_DATA_DCC_FMC_CH7,
                             WVF_DATA_DCC_FMC_ALL},
+                           {-1,
+                            -1,
+                            -1,
+                            -1,
+                            -1,
+                            -1,
+                            -1,
+                            -1,
+                            -1},
                           },
     /* [CH_DCC_P2P] =  */ {CH_HW_DCC_P2P,                       // HwDataChannel
                             {WVF_DATA_DCC_P2P_CH0,
@@ -89,6 +98,15 @@ static const channelMap_t channelMap[CH_END] = {
                             WVF_DATA_DCC_P2P_CH6,
                             WVF_DATA_DCC_P2P_CH7,
                             WVF_DATA_DCC_P2P_ALL},
+                            {-1,
+                            -1,
+                            -1,
+                            -1,
+                            -1,
+                            -1,
+                            -1,
+                            -1,
+                            -1},
                           },
 };
 
