e9e8316cf7d55a78d86cf0b19a92aa5896847fc0
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2022-03-22 08:13:08 -0300

    scripts/python: update slot mapping generator script to support crate devel

--- scripts/python/slot_mapping_generator.py
+++ scripts/python/slot_mapping_generator.py
@@ -1,7 +1,5 @@
 f1 = open("slot-mapping.txt", "w+")
 
-#Formato: SI-XX:DI-FOFBCtrl
-
 crates = 22
 slots = 24
 ioc_name = "FOFB"
@@ -42,13 +40,30 @@ for i in range(1, crates+1, 1):
             f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_AREA_PREFIX=" + pv_area_prefix_others + "-" + str(i) + "SL" + str(j) + '\n')
             f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_DEVICE_PREFIX=" + pv_device_prefix + "-" + pv_name_prefix + ":" + '\n'+ '\n')
 
+f1.write('\n' + "# --- CRATE 98 (devel) ---" + '\n' + '\n')
+for j in range(1, slots+1, 1):
+  i = 98
+  if (j == 3 or j == 5):
+    f1.write("# Crate " + str(i) + " - " + ioc_name + " slot " + str(j) + " - " + pv_area_prefix + "-" + str(i) + pv_device_prefix + "-" + pv_name_prefix + '\n')
+    f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_AREA_PREFIX=" + pv_area_prefix + "-" + str(i) + '\n')
+    f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_DEVICE_PREFIX=" + pv_device_prefix + "-" + pv_name_prefix + ":" + '\n'+ '\n')
+  else:
+    if j < 10:
+      f1.write("# Crate " + str(i) + " - " + ioc_name + " slot " + str(j) + " - " + pv_area_prefix_others + "-" + str(i) + "SL0" + str(j) + pv_device_prefix + "-" + pv_name_prefix + '\n')
+      f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_AREA_PREFIX=" + pv_area_prefix_others + "-" + str(i) + "SL0" + str(j) + '\n')
+      f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_DEVICE_PREFIX=" + pv_device_prefix + "-" + pv_name_prefix + ":" + '\n'+ '\n')
+    else:
+      f1.write("# Crate " + str(i) + " - " + ioc_name + " slot " + str(j) + " - " + pv_area_prefix_others + "-" + str(i) + "SL" + str(j) + pv_device_prefix + "-" + pv_name_prefix + '\n')
+      f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_AREA_PREFIX=" + pv_area_prefix_others + "-" + str(i) + "SL" + str(j) + '\n')
+      f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_DEVICE_PREFIX=" + pv_device_prefix + "-" + pv_name_prefix + ":" + '\n'+ '\n')
+
 
 f1.write('\n' + "# --- CRATE 99 (minicrate) ---" + '\n' + '\n')
 for j in range(1, slots+1, 1):
   i = 99
   if (j == 3 or j == 5):
     f1.write("# Crate " + str(i) + " - " + ioc_name + " slot " + str(j) + " - " + pv_area_prefix + "-" + str(i) + pv_device_prefix + "-" + pv_name_prefix + '\n')
-    f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_AREA_PREFIX=" + pv_area_prefix + "-" + str(i) + ":" + '\n')
+    f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_AREA_PREFIX=" + pv_area_prefix + "-" + str(i) + '\n')
     f1.write("CRATE_" + str(i) + "_" + ioc_name + "_" + str(j) + "_PV_DEVICE_PREFIX=" + pv_device_prefix + "-" + pv_name_prefix + ":" + '\n'+ '\n')
   else:
     if j < 10:
