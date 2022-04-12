2a041b6783fbcbe2fd18db4c588d82e074d9a3bf
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2022-04-12 16:32:21 -0300

    scripts/python: update script for reformat fofb packet

--- scripts/python/reformat_fofb_packet_16b.py
+++ scripts/python/reformat_fofb_packet_16b.py
@@ -17,10 +17,10 @@ p = re.compile(LINE_PATTERN)
 try:
   data_format = sys.argv[1]
 except IndexError:
-  data_format = "uint16"
+  data_format = "int32"
 
 i = 0
-print("| {:8} | {:10} | {:8} | {:6} |  {:^6}  |  {:^6}  | {:4} |  {:7} |  {:9} |  {:10} |  {:10} |  {:10} |  {:10} |  {:10} |  {:10} |".
+print("| {:8} | {:^10} | {:^8} | {:^6} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} |".
   format("packet #", "tf_cntr_16", "tf_start", "bpm_id", "bpm_x", "bpm_y", "tf_cntr_32", "sp_x_0", "sp_y_0", "sp_x_1", "sp_y_1",  "sp_x_2", "sp_y_2", "sp_x_3", "sp_y_3"))
 
 for line in sys.stdin:
@@ -39,7 +39,7 @@ for line in sys.stdin:
   aux_sp_3 = atoms[7]
 
   if data_format == "uint16":
-    print("| {:<8} | {:<10} | {:<8} | {:<6} | {:<8} | {:<8} | {:<10} | {:<8} | {:10} |  {:<10} |  {:<10} |  {:<10} |  {:<10} |  {:<10} |  {:<10} |".
+    print("| {:8} | {:^10} | {:^8} | {:^6} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} |".
       format(i, tf_cntr_16_lsb, tf_start, bpm_id,
         (conv_int32_to_uint32(bpm_pos_x) & 0xFFFF0000) >> 16,
         (conv_int32_to_uint32(bpm_pos_x) & 0xFFFF),
@@ -56,7 +56,7 @@ for line in sys.stdin:
         (conv_int32_to_uint32(aux_sp_3) & 0xFFFF)
         ))
   elif data_format == "int32":
-    print("| {:<8} | {:<10} | {:<8} | {:<6} |  {:<10}   |  {:<10}   | {:<10} |  {:<10} |  {:<10} |  {:<10} |  {:<10} |  {:<10} |  {:<10} |  {:<10} |  {:<10} |".
+    print("| {:8} | {:^10} | {:^8} | {:^6} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} | {:^10} |".
       format(i, tf_cntr_16_lsb, tf_start, bpm_id,
         bpm_pos_x,
         bpm_pos_y,
@@ -71,5 +71,3 @@ for line in sys.stdin:
         (conv_int32_to_uint32(aux_sp_3) & 0xFFFF)
         ))
   i = i + 1
-
-
