a9486d96ab84f8d35343cf6754976085d5f419e8
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2022-04-13 16:52:00 -0300

    scripts/python: add macros to fofb_gui.ui and run_fofb.sh

--- scripts/python/run_fofb_gui.sh
+++ scripts/python/run_fofb_gui.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-pydm --hide-nav-bar --hide-menu-bar --hide-status-bar fofb_gui.ui
+pydm -m 'SECTION="XX", CRATE="99", SLOT="SL01"' --hide-nav-bar --hide-menu-bar --hide-status-bar fofb_gui.ui
+
+# if the board is connected in physical slot 02 or 03, SECTION="IA" and SLOT=""
+# else, we have SECTION="XX" and SLOT="SLn", where n=2*physical_slot-1 (n has two digits)
