0c469168ecff6129e53dbf0c90cf8d63f86ba57a
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2022-04-29 15:09:38 -0300

    scripts/python: remove ununsed files and update ram coefficients script

--- scripts/python/update_ram_coefficients.py
+++ scripts/python/update_ram_coefficients.py
@@ -12,6 +12,7 @@
 from epics import PV
 import numpy as np
 from time import sleep
+from fxpmath import Fxp
 import sys
 
 # constants
@@ -20,14 +21,24 @@ slot_number    = sys.argv[2]      # FOFB slot number (physical_slot*2-1 = two di
 channels       = 8                # number of FOFB Processing channels
 ram_size       = 2**8             # size of RAM fixed in gateware
 addrs_in_use   = 160              # size of RAM actually in use
-ram_addr       = []               # list with all addresses in use
+ram_addr       = []               # list with RAM addresses in use
+ram_data_in    = []               # list with RAM coefficients
 
 for j in range(0, channels):
   for i in range(j*ram_size, j*ram_size + addrs_in_use):
     ram_addr.append(i)
 
-# load file with RAM coefficients for all channels (size = channels*addrs_in_use)
-ram_data_in    = np.loadtxt('ram_data_in.txt') 
+# load coefficients
+RespMat_h = np.loadtxt('sirius_SOFB_RespMat_Horizontal_Plane.txt')
+RespMat_v = np.loadtxt('sirius_SOFB_RespMat_Vertical_Plane.txt')
+
+InvRespMat_h = np.loadtxt('sirius_SOFB_InvRespMat_Horizontal_Plane.txt')
+InvRespMat_v = np.loadtxt('sirius_SOFB_InvRespMat_Vertical_Plane.txt')
+
+# generate file with RAM coefficients for all channels (size = channels*addrs_in_use)
+for i in InvRespMat_h[0][:]:
+  s = Fxp(i, signed=True, n_word=32, n_frac=26)
+  ram_data_in.append(s.raw())
 
 # PV prefixes
 if slot_number == ("03" or "05"): # board connected in physical slot 2 or 3
