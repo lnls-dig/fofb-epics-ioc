caf4d8978a64c196589773bba5da9a7cd6954fbe
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2022-05-17 10:20:35 -0300

    scripts/python: fix script to update ram coefficients

--- scripts/python/RAM/update_ram_coefficients.py
+++ scripts/python/RAM/update_ram_coefficients.py
@@ -15,11 +15,13 @@ from fxpmath import Fxp
 
 # constants
 channels       = 8                # number of FOFB Processing channels
-ram_size       = 2**channels      # size of RAM fixed in gateware
-addrs_in_use   = 160              # size of RAM actually in use
+n_bits         = 32               # number of bits for RAM data
+n_fix          = 26               # fixed point (the same value that we set in gateware)
+ram_size       = 2**channels      # size of RAM (the same value tha we set in gateware)
+addrs_in_use   = 80               # number of coefficients (80 horizontal and 80 vertical)
 ram_addr       = []               # list with RAM addresses in use
 ram_data_in    = []               # list with RAM coefficients
-crates         = ["01", "02", "03"]
+crates         = ["99"] #["01", "02", "03", "04", "05", "06", "07", "08", "09", "10",                                                                                                               "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 
 print('\n')
 print('         # # # # # # # # # # # # # # # # # # # # # # # # # #')
@@ -36,22 +38,23 @@ for j in range(0, channels):
     ram_addr.append(i)
 
 # load coefficients
-InvRespMat_h  = np.loadtxt('sirius_SOFB_InvRespMat_Vertical_Plane.txt') # find the correct coefficients and change here
+InvRespMat_h  = np.loadtxt('sirius_SOFB_InvRespMat_Horizontal_Plane.txt') # find the correct coefficients and change here
 InvRespMat_v  = np.loadtxt('sirius_SOFB_InvRespMat_Vertical_Plane.txt')
 
-# generate list with RAM coefficients for all channels
+# generate list with RAM coefficients for all racks 
 for j in range(0, addrs_in_use):
 	for i in InvRespMat_h[j][:]:
-		s = Fxp(i, signed=True, n_word=32, n_frac=26)
+		s = Fxp(i, signed=True, n_word=n_bits, n_frac=n_fix)
 		ram_data_in.append(s.raw())
 	for i in InvRespMat_v[j][:]:
-		s = Fxp(i, signed=True, n_word=32, n_frac=26)
+		s = Fxp(i, signed=True, n_word=n_bits, n_frac=n_fix)
 		ram_data_in.append(s.raw()) 
 
 cnt = 0
 for crate_number in crates:
 	print('>>> Update RAM coefficients for crate ', crate_number)
-	pv_prefix    = "IA-" + crate_number + "RaBPM:BS-FOFBCtrl:"
+	#pv_prefix    = "IA-" + crate_number + "RaBPM:BS-FOFBCtrl:"
+	pv_prefix    = "XX-" + crate_number + "SL07RaBPM:BS-FOFBCtrl:"
 	addr         = PV(pv_prefix + 'FofbProcessingRamAddr-SP')
 	data_in      = PV(pv_prefix + 'FofbProcessingRamDataIn-SP')
 	write_en     = PV(pv_prefix + 'FofbProcessingRamWriteEnable-SP')
